89714dc092cdb145f0511b2c0ab800535bc6cf24
lWillian <willian.vlcs@gmail.com>
2021-08-26 23:57:22 -0300

    Implementação ViewModel

---
+++ app/src/main/java/com/example/vmodel/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.vmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private var contador: Int = 0
+
+    var mContador = MutableLiveData<String>().apply {
+        value = contador.toString()
+    }
+
+
+    private fun setmContador(){
+      mContador.value = contador.toString()
+    }
+
+    private fun validaContador() {
+        contador++
+        if (contador > 5) {
+            contador = 0
+        }
+        setmContador()
+    }
+
+    fun Contador(){
+        validaContador()
+    }
+
+
+}
